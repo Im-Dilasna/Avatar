b57341c647adb90276cec06e9a31fbfab1a74e39
NoobultraPro-Max <97903934+NoobultraPro-Max@users.noreply.github.com>
2022-01-24 01:48:58 -0800

    Create alive.py

---
+++ Avatar/modules/alive.py
@@ -0,0 +1,24 @@
+import os
+import re
+from platform import python_version as kontol
+from telethon import events, Button
+from telegram import __version__ as telever
+from telethon import __version__ as tlhver
+from pyrogram import __version__ as pyrover
+from Avatar.events import register
+from Avatar import telethn as tbot
+
+
+PHOTO = "https://telegra.ph/file/ff2fa22dfa6ae838cc6cd.jpg"
+
+@register(pattern=("/alive"))
+async def awake(event):
+  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Avatar </𝙎𝙉𝙏</> 🇱🇰.** \n\n"
+  TEXT += "💠 **I'm Working Properly** \n\n"
+  TEXT += f"💠 **My Master : [Noob](https://t.me/Slninjateam)** \n\n"
+  TEXT += f"💠 **Library Version :** `{telever}` \n\n"
+  TEXT += f"💠 **Telethon Version :** `{tlhver}` \n\n"
+  TEXT += f"💠 **Pyrogram Version :** `{pyrover}` \n\n"
+  TEXT += "**Thanks For Adding Me Here ❤️**"
+  BUTTON = [[Button.url("Help", "https://t.me/@miss_Alexie_bot?start=help"), Button.url("Support", "https://t.me/slninjateam")]]
+  await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
