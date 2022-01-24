c76431c7f26719e9b7676e45f4b63bfd54caec20
Dilasnaofficical <98165475+Dilasnaofficical@users.noreply.github.com>
2022-01-24 14:12:10 +0530

    adeh

---
+++ Avatar/modules/yt.py
@@ -0,0 +1,7 @@
+﻿__help__ = """
+ - /yt <text>: perform a youtube search
+ - /ytaudio <link> or /ytvideo <link>: Downlods a video or audio from a youtube video to the bots local server
+ 
+Avatar
+"""
+__mod_name__ = "Youtube 🎞"
